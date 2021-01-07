2c5f11404c60df872e7ff38ac97d24131a43374e
sendak <sendak.vivian.mahr@gmail.com>
2021-01-06 21:43:44 -0800

    initial commit

---
+++ prep_posts.py
@@ -0,0 +1,19 @@
+"""
+if there's something in current
+	that's your working photo, pull desc and work on it
+else: 
+	pick something random from archive, pull desc, and process it
+
+min of 1080 width for insta, 500 for twitter
+	# dunno how to handle animated gifs yet
+	create
+	jpg for insta, png for twitter
+
+	go on twitter and insta
+
+	log in
+
+	set up posts
+
+	input to close
+"""
